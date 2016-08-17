610cb961f08a64b90a4f44838da99531835b0053
Michael Joyce <ubermichael@gmail.com>
2016-08-17 17:22:39 +0000

    progress. slow glacial geologic progress.

--- theme/islandora-ohms.tpl.php
+++ theme/islandora-ohms.tpl.php
@@ -7,27 +7,9 @@
  * @TODO: add documentation about file and available variables
  */
 ?>
+<script type="text/javascript">
+var cachefile = 'http://techbc.vbox:8000/islandora/object/islandora%3A2/datastream/OBJ/view';
+var playerName = 'other';  
+</script>
 
-<div class="islandora-ohms-object islandora" vocab="http://schema.org/" prefix="dcterms: http://purl.org/dc/terms/" typeof="AudioObject">
-  <div class="islandora-ohms-content-wrapper clearfix">
-    <?php if (isset($islandora_content)): ?>
-      <div class="islandora-ohms-content">
-        <?php print $islandora_content; ?>
-      </div>
-    <?php endif; ?>
-  </div>
-  <div class="islandora-ohms-metadata">
-    <?php print $description; ?>
-    <?php if ($parent_collections): ?>
-      <div>
-        <h2><?php print t('In collections'); ?></h2>
-        <ul>
-          <?php foreach ($parent_collections as $collection): ?>
-            <li><?php print l($collection->label, "islandora/object/{$collection->id}"); ?></li>
-          <?php endforeach; ?>
-        </ul>
-      </div>
-    <?php endif; ?>
-    <?php print $metadata; ?>
-  </div>
-</div>
+<?php print $islandora_content; ?>
