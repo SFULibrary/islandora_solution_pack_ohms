7c648e61b84df85b38e5c867279926f7966b8545
Michael Joyce <ubermichael@gmail.com>
2016-09-01 16:50:35 +0000

    prototype the metadata building process.

--- theme/islandora-ohms.tpl.php
+++ theme/islandora-ohms.tpl.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * @file
  * This is the template file for the object page for ohms file.
@@ -11,20 +10,21 @@
   var cachefile = "<?php echo $islandora_object->id ?>";
   var searchUrl = "<?php echo $search_url; ?>";
 </script>
+
 <div id='ohms_body'>
-	<?php print $islandora_content; ?>
+    <?php print $islandora_content; ?>
 </div>
 <div class="islandora-ohms-metadata">
-	<?php print $description; ?>
-	<?php if ($parent_collections): ?>
-		<div>
-	        <h2><?php print t('In collections'); ?></h2>
-	        <ul>
-				<?php foreach ($parent_collections as $collection): ?>
-		            <li><?php print l($collection->label, "islandora/object/{$collection->id}"); ?></li>
-				<?php endforeach; ?>
-	        </ul>
-		</div>
-	<?php endif; ?>
-	<?php print $metadata; ?>
+    <?php print $description; ?>
+    <?php if ($parent_collections): ?>
+      <div>
+          <h2><?php print t('In collections'); ?></h2>
+          <ul>
+              <?php foreach ($parent_collections as $collection): ?>
+                <li><?php print l($collection->label, "islandora/object/{$collection->id}"); ?></li>
+              <?php endforeach; ?>
+          </ul>
+      </div>
+    <?php endif; ?>
+    <?php print $metadata; ?>
 </div>
